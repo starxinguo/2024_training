d8d5bbf9a4c4eef7c059d89693bf617cb730779f
guoxin <2306695487@qq.com>
2024-06-20 21:04:35 +0800

    7

--- 第四周/single-vehicle-side-example/read.py
+++
@@ -1,7 +0,0 @@
-import base64
-
-with open('image/000000.jpg', 'rb') as image_file:
-    image_data = image_file.read()
-    image_base64 = base64.b64encode(image_data).decode('utf-8')
-    image_url = f'data:image/jpeg;base64,{image_base64}'
-    print(image_url)
